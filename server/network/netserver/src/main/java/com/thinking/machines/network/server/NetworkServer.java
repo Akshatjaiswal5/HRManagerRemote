ad588acf93f583d54f4203839e0aef5986327714
DESKTOP-T526RFM\lenovo <akshatjaiswal521@gmail.com>
2021-12-21 20:02:22 +0530

    continue work

--- server/network/netserver/src/main/java/com/thinking/machines/network/server/NetworkServer.java
+++ server/network/netserver/src/main/java/com/thinking/machines/network/server/NetworkServer.java
@@ -1,3 +1,15 @@
 package com.thinking.machines.network.server;
+import com.thinking.machines.network.common.*;
+import com.thinking.machines.network.common.execptions.*;
 public class NetworkServer {
+  private RequestHanderInterface requestHandler;
+  public NetworkServer(RequestHanderInterface requestHandler) throws NetworkException{
+   if(requestHandler==null)
+   throw new NetworkException("Request Handler Missing");
+   this.requestHandler=requestHandler;
+  }
+
+  public void start(){
+   
+  }
 }
---
+++ server/network/netserver/src/main/java/com/thinking/machines/network/server/RequestHander.java
@@ -0,0 +1,5 @@
+package com.thinking.machines.network.server;
+import com.thinking.machines.network.common.*;
+public interface RequestHanderInterface {
+ public Response process(Request request);
+}
